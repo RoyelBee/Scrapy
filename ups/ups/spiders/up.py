cb86709619be3e71c0b81adea5d4f6fc385278ee
Royel <rejaul35-910@diu.edu.bd>
2020-12-07 23:51:18 +0600

    fetch data from ups

--- ups/ups/spiders/up.py
+++ ups/ups/spiders/up.py
@@ -1,4 +1,12 @@
 import scrapy
+from selenium import webdriver
+from scrapy.selector import Selector
+from shutil import which
+from selenium.webdriver.common.keys import Keys
+# for Headless
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import time
 
 
 class UpSpider(scrapy.Spider):
@@ -7,4 +15,46 @@ class UpSpider(scrapy.Spider):
     start_urls = ['http://ups.com/']
 
     def parse(self, response):
-        pass
+        df = pd.read_csv('G:/Scrapy/ups/tracking_id.csv')
+        ids = df['id']
+
+        for id in ids:
+            print('\n', id, '\n')
+
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_path = which("chromedriver")
+
+            # driver = webdriver.Chrome(executable_path=chrome_path)
+            driver = webdriver.Chrome(executable_path=chrome_path)
+            driver.get("https://www.ups.com/track?loc=en_US&requester=ST/")
+            time.sleep(10)
+            search_input = driver.find_element_by_id("stApp_trackingNumber")
+            search_input.send_keys(id)
+
+            search_btn = driver.find_element_by_id("stApp_btnTrack")
+            search_btn.click()
+
+            time.sleep(10)
+            delivery = driver.find_element_by_id('stApp_txtPackageStatus')
+            date = driver.find_element_by_id('stApp_deliveredDate')
+            day = driver.find_element_by_id('stApp_deliveredDay')
+            timef = driver.find_element_by_id('stApp_eodDate')
+
+            deliverState = driver.find_element_by_id('stApp_txtAddress')
+            deliverCountry = driver.find_element_by_id('stApp_txtCountry')
+            leftat = driver.find_element_by_id('stApp_txtLeftAt')
+            Received = driver.find_element_by_id('stApp_txtReceivedBy')
+
+            yield {
+                'Status': delivery.text,
+                'Date': date.text,
+                'Day': day.text,
+                'Time': timef.text,
+                'Delevery Location': deliverState.text + deliverCountry.text,
+                'Left At': leftat.text,
+                'Receiver': Received.text
+            }
+            driver.close()
+
+        print("End\n\n")
--- waldometers/waldometers/spiders/countries.py
+++ waldometers/waldometers/spiders/countries.py
@@ -1,11 +1,11 @@
 import scrapy
 import logging
 
+
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
-
 
     def parse(self, response):
         countries = response.xpath('//td/a')
@@ -19,21 +19,18 @@ class CountriesSpider(scrapy.Spider):
             # yield scrapy.Request(url=absoulate_url)
 
             # Another way
-            yield response.follow(url=country_url, callback=self.parse_countries, meta={'country_name' : country_name})
+            yield response.follow(url=country_url, callback=self.parse_countries, meta={'country_name': country_name})
 
     def parse_countries(self, response):
         name = response.request.meta['country_name']
-        urls = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        urls = response.xpath(
+            '(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
         for url in urls:
             year = url.xpath(".//td[1]/text()").get()
             population = url.xpath(".//td[2]/strong/text()").get()
 
             yield {
                 'Country': name,
-                'Year' : year,
+                'Year': year,
                 'Population': population
             }
-
-
-
-
